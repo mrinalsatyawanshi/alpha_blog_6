cd670ec92b16552cf1ccb8376a21e2776d955f8a
mrinalsatyawanshi <mrinal.satyawanshi@anchanto.com>
2022-01-23 16:00:10 +0530

    Add user login and set sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-    
+    before_action :set_user, only: [:show, :edit, :update]
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end
-    
+
     def new
         @user = User.new
     end
@@ -16,6 +15,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome #{@user.username}, to the Alpha Blog. You have succesfully signed up!!"
             redirect_to articles_path
         else
@@ -24,11 +24,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+     
     end
 
     def update
-        @user = User.find(params[:id])
+       
         if @user.update(user_params)
             flash[:notice] = "You have successfully updated your account"
             redirect_to @user
@@ -41,5 +41,9 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username,:email,:password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
