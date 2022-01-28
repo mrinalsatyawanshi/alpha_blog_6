7f0d9e9bf0002c6d77e9b1e53729c7d008f73350
mrinalsatyawanshi <mrinal.satyawanshi@anchanto.com>
2022-01-28 20:07:08 +0530

    create migration file and create many to many association

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
     def new
         @category = Category.new
     end
@@ -19,11 +20,19 @@ class CategoriesController < ApplicationController
     end
 
     def index
+        @categories = Category.paginate(page: params[:page], per_page: 5)
     end
 
     private
 
     def category_params
         params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert] = "Only admins can perfom this action"
+            redirect_to categories_path
+        end
     end
 end
